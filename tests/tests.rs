05dd2404b509297a2c097aa7589f2aaaf644e216
jofas <jonas@fc-web.de>
2022-09-29 14:14:45 +0200

    initial commit

---
+++ tests/tests.rs
@@ -0,0 +1,12 @@
+use actix_playground::{MyActor, Ping};
+
+use actix::Actor;
+
+#[actix_rt::test]
+async fn test() {
+  let addr = MyActor { count: 10 }.start();
+
+  let res = addr.send(Ping(10)).await;
+
+  assert_eq!(res.unwrap(), 20);
+}
