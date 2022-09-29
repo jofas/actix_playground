57413bd3cea7547ec6d3c67f637c05a2f575ad27
jofas <jonas@fc-web.de>
2022-09-29 15:06:21 +0200

    actor communications

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,20 @@
-use actix::{Actor, Context, Handler, Message};
+use actix::{Actor, Context, Handler, Message, Recipient};
+
+use jonases_tracing_util::tracing::{event, Level};
 
 #[derive(Message)]
-#[rtype(result = "usize")]
+#[rtype(result = "()")]
 pub struct Ping(pub usize);
 
 pub struct MyActor {
-  pub count: usize,
+  id: usize,
+  subscribers: Vec<Recipient<Ping>>,
+}
+
+impl MyActor {
+  pub fn new(id: usize, subscribers: Vec<Recipient<Ping>>) -> Self {
+    Self { id, subscribers }
+  }
 }
 
 impl Actor for MyActor {
@@ -13,14 +22,18 @@ impl Actor for MyActor {
 }
 
 impl Handler<Ping> for MyActor {
-  type Result = usize;
+  type Result = ();
 
-  fn handle(
-    &mut self,
-    msg: Ping,
-    _ctx: &mut Context<Self>,
-  ) -> Self::Result {
-    self.count += msg.0;
-    self.count
+  fn handle(&mut self, msg: Ping, _ctx: &mut Context<Self>) {
+    event!(
+      Level::ERROR,
+      "Actor: {} just got pinged from {}",
+      self.id,
+      msg.0
+    );
+
+    for s in &self.subscribers {
+      s.do_send(Ping(self.id));
+    }
   }
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -2,11 +2,23 @@ use actix_playground::{MyActor, Ping};
 
 use actix::Actor;
 
+#[allow(unused_imports)]
+use jonases_tracing_util::tracing::{event, Level};
+
 #[actix_rt::test]
 async fn test() {
-  let addr = MyActor { count: 10 }.start();
+  jonases_tracing_util::init_logger();
 
-  let res = addr.send(Ping(10)).await;
+  let addr4 = MyActor::new(4, vec![]).start();
 
-  assert_eq!(res.unwrap(), 20);
+  let addr3 = MyActor::new(3, vec![addr4.recipient()]).start();
+
+  let addr2 =
+    MyActor::new(2, vec![addr3.clone().recipient()]).start();
+
+  let addr1 =
+    MyActor::new(1, vec![addr2.recipient(), addr3.recipient()])
+      .start();
+
+  addr1.send(Ping(0)).await.unwrap();
 }
