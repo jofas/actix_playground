848923353c2ce203ab3a66a0f8f5ee775604b5cf
jofas <jonas@fc-web.de>
2023-01-06 12:48:48 +0100

    started working on gql api tests

--- tauri/src/src/api.rs
+++ tauri/src/src/api.rs
@@ -4,7 +4,14 @@ use async_graphql::{InputObject, SimpleObject};
 
 #[derive(FromRow, SimpleObject, InputObject, Debug, PartialEq)]
 pub struct Entry {
-  pub id: Option<u32>,
+  pub id: u32,
+  pub title: String,
+  pub body: String,
+  pub published: bool,
+}
+
+#[derive(InputObject, Debug)]
+pub struct CreateEntry {
   pub title: String,
   pub body: String,
   pub published: bool,
