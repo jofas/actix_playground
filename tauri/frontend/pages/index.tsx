44e27831125fc93e96e4b6ec151f906075817a9f
jofas <jonas@fc-web.de>
2023-01-09 15:39:52 +0100

    basic communcation between rust and ts established. Now this has to be replaced with a typed solution based on generated gql types

--- tauri/frontend/pages/index.tsx
+++ tauri/frontend/pages/index.tsx
@@ -6,18 +6,26 @@ import { useState, useEffect } from 'react'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const [greeting, setGreeting] = useState("")
+  const [projects, setProjects] = useState([])
 
   useEffect(() => {
-    invoke('greet', { name: 'World' })
-      .then(setGreeting)
+    invoke('graphql', { query: "{projects}" })
+      .then((projects) => {
+        setProjects(projects);
+      })
+      .catch((e) => {
+        // bad workaround for broken webkit inspector
+        setProjects(e);
+      })
   })
 
   return (
     <>
       <main className={styles.main}>
         <h1>Example Tauri App</h1>
-        <p>{greeting}</p>
+        <p>
+          projects: { JSON.stringify(projects) }
+        </p>
       </main>
     </>
   )
